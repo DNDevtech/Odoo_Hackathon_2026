b70f810c7baa1270f85947d0575221d601026212
kaushikbigscal <154492517+kaushikbigscal@users.noreply.github.com>
2026-07-12 07:18:08 +0000

    all module update

---
+++ transitops/models/__init__.py
@@ -0,0 +1,6 @@
+from . import transitops_vehicle
+from . import transitops_driver
+from . import transitops_trip
+from . import transitops_maintenance
+from . import transitops_fuel_log
+from . import transitops_expense
